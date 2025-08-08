cf0135be17350779401917f2ba46b3df0227a3bc
SheepYhangCN <sheepyuhang@163.com>
2025-08-08 20:49:28 +0800

    update cc 4.0

--- traditionalize.py
+++ traditionalize.py
@@ -8,6 +8,7 @@ replaces = {
 	r'<ul class="list-inline">你现在正在浏览的是简体中文站。<br>点击前往<a class="font-w-bold" href="https://cht.cht.utclc.top" target="_self">繁体中文站</a>（实验性）。</ul>':
 	r'<ul class="list-inline">你現在正在繁體中文站，此站點為實驗性，可能會有潛在的問題。<br>請透過 <a class="font-w-bold" href="https://github.com/UTCLC/website_cht/issues" target="_blank">Github Issue</a> 回報你遇到的錯誤。<br>点击返回<a class="font-w-bold" href="https://utclc.top" target="_self">簡體中文站</a>。</ul>',
 	"zh-CN":"zh-TW",
+	"zh-hans":"zh-hant",
 	"扩展":"拓展",
 	"本地化":"在地化",
 	"“":"「",
